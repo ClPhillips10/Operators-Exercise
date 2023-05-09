207c25b14b85d9905b462128800af4508c9be865
ClPhillips10 <Lakephillips3232@gmail.com>
2023-05-09 05:09:17 -0500

    Exercise one

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var a = 17;
+            var b = 4;
+            int quotient = (a / b);
+            int Remainder = (a % b);
+
+
+            int Radius = 3;
+
+            if (a == 17 && b == 4) 
+            {
+                Console.WriteLine($"17/4 is {quotient} remainder {Remainder} ");
+            }
         }
     }
 }
